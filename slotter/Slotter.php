bf02ada64df2e51308e408fcb76c44a837fc27e0
André Luiz Girol <andregirol@gmail.com>
2018-09-05 18:03:27 -0300

    Refatorando variáveis e protótipo de alocação de nichos

--- slotter/Slotter.php
+++ slotter/Slotter.php
@@ -2,11 +2,11 @@
 
 class Slotter
 {
-    private $nicho_medio = [];
-    private $nicho_grande = [];
+    private $nichos_medios = [];
+    private $nichos_grandes = [];
 
-    // inicializa quantos nichos existem
     /**
+     *  Inicializa quantos nichos existem
      * @param int $qtd_medio Quantidade de nichos Médios disponíveis
      * @param int $qtd_grande Quantidade de nichos Grandes disponíveis
      * 
@@ -16,8 +16,8 @@ class Slotter
     {
         $this->aloca_nichos($qtd_medio, $qtd_grande, $num_slots);
         // TESTING
-        var_dump($this->nicho_medio);
-        var_dump($this->nicho_grande);
+        var_dump($this->nichos_medios);
+        var_dump($this->nichos_grandes);
     }
     /**
      * Aloca em memória quantos nichos existem
@@ -33,10 +33,34 @@ class Slotter
             'M2' = ['0','0','0','0','0','0','0','0',]
         ]
         
-        */ 
+         */
         for ($i = 1; $i <= $qtd_medio; $i++) {
-            $this->nicho_medio['M' . $i] = array_fill(0, $num_slots, '0');
-            $this->nicho_grande['G' . $i] = array_fill(0, $num_slots, '0');
+            $this->nichos_medios['M' . $i] = array_fill(0, $num_slots, '0');
+            $this->nichos_grandes['G' . $i] = array_fill(0, $num_slots, '0');
         }
     }
+
+    /**
+     * reserva slots disponíveis em um nicho
+     * 
+     * @param string $tam_nicho 'medio' ou 'grande'
+     * @param int $qtd_slots Quantos slots precisa?
+     */
+    public function reserva_slots_nicho(string $tam_nicho, int $qtd_slots)
+    {
+        if ($tam_nicho == 'medio') {
+            $nichos = $this->nichos_medios;
+        } else {
+            $nichos = $this->nichos_grandes;
+        }
+
+        foreach ($nichos as $nicho) {
+
+        }
+    }
+
+    public function aloca_slot()
+    {
+    }
+
 }
