9c52c271fabcab77fa448d5e399e809b60d3b816
André Luiz Girol <andregirol@gmail.com>
2018-09-05 17:05:56 -0300

    Refatorando para utilizar mesma função de alocar

--- slotter/Slotter.php
+++ slotter/Slotter.php
@@ -7,24 +7,27 @@ class Slotter
 
     // inicializa quantos nichos existem
     /**
-     * @param int $tam_medio Quantidade de nichos Médios disponíveis
-     * @param int $tam_grande Quantidade de nichos Grandes disponíveis
+     * @param int $qtd_medio Quantidade de nichos Médios disponíveis
+     * @param int $qtd_grande Quantidade de nichos Grandes disponíveis
      * 
      * @return void
      */
-    public function __construct($qtd_medio = 2, $tam_grande = 2)
+    public function __construct($qtd_medio = 2, $qtd_grande = 2)
     {
-        $this->aloca_medios($qtd_medio);
+        $this->aloca_nichos($qtd_medio, $qtd_grande);
         var_dump($this->nicho_medio);
+        var_dump($this->nicho_grande);
     }
     /**
-     * Aloca em memória quantos nichos médios existem
-     * @param int $tam_medio Quantidade de nichos Médios disponíveis
+     * Aloca em memória quantos nichos existem
+     * @param int $qtd_medio Quantidade de nichos Médios disponíveis
+     * @param int $qtd_grande Quantidade de nichos Grandes disponíveis
      */
-    private function aloca_medios($qtd_medio)
+    private function aloca_nichos($qtd_medio, $qtd_grande)
     {
         for ($i=1; $i <= $qtd_medio ; $i++) { 
             $this->nicho_medio['M'. $i] = array();
+            $this->nicho_grande['G'. $i] = array();
         }
     }
 }
